0411ba8e5a1910bb6296941201366e4dbf656731
vfurmane <vfurmane@student.42.fr>
2022-03-12 11:39:02 +0100

    feat: add reverse_iterator prototype

---
+++ reverse_iterator.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_iterator.hpp                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/12 09:49:43 by vfurmane          #+#    #+#             */
+/*   Updated: 2022/03/12 09:52:12 by vfurmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef REVERSE_ITERATOR_HPP
+# define REVERSE_ITERATOR_HPP
+
+namespace ft
+{
+	template <class Iter>
+	class reverse_iterator
+	{
+	};
+};
+
+#endif
--- vector.hpp
+++ vector.hpp
@@ -6,25 +6,35 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 12:05:21 by vfurmane          #+#    #+#             */
-/*   Updated: 2022/03/11 15:30:24 by vfurmane         ###   ########.fr       */
+/*   Updated: 2022/03/12 11:38:33 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VECTOR_HPP
 # define VECTOR_HPP
 
+# include "reverse_iterator.hpp"
+
 namespace ft
 {
+	template <class T>
+	class _vector_iterator {
+	};
+
 	template < class T, class Alloc = std::allocator<T> >
 	class vector
 	{
 		public:
-			typedef T value_type;
-			typedef Alloc allocator_type;
-			typedef value_type &reference;
-			typedef value_type const &const_reference;
-			typedef value_type *pointer;
-			typedef value_type const *const_pointer;
+			typedef	T									value_type;
+			typedef Alloc								allocator_type;
+			typedef value_type							&reference;
+			typedef value_type							const &const_reference;
+			typedef value_type							*pointer;
+			typedef value_type							const *const_pointer;
+			typedef _vector_iterator<T>					iterator;
+			typedef _vector_iterator<const T>			const_iterator;
+			typedef ft::reverse_iterator<iterator>			reverse_iterator;
+			typedef ft::reverse_iterator<const_iterator>	const_reverse_iterator;
 	};
 };
 
