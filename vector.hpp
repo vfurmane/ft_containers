8a41d350c0daa46c209f6e0a59759cd6f02a9dd0
vfurmane <vfurmane@student.42.fr>
2022-03-12 12:08:28 +0100

    style: fix indentation

--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 12:05:21 by vfurmane          #+#    #+#             */
-/*   Updated: 2022/03/12 11:38:33 by vfurmane         ###   ########.fr       */
+/*   Updated: 2022/03/12 12:08:16 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,14 @@ namespace ft
 	class vector
 	{
 		public:
-			typedef	T									value_type;
-			typedef Alloc								allocator_type;
-			typedef value_type							&reference;
-			typedef value_type							const &const_reference;
-			typedef value_type							*pointer;
-			typedef value_type							const *const_pointer;
-			typedef _vector_iterator<T>					iterator;
-			typedef _vector_iterator<const T>			const_iterator;
+			typedef	T										value_type;
+			typedef Alloc									allocator_type;
+			typedef value_type								&reference;
+			typedef value_type								const &const_reference;
+			typedef value_type								*pointer;
+			typedef value_type								const *const_pointer;
+			typedef _vector_iterator<T>						iterator;
+			typedef _vector_iterator<const T>				const_iterator;
 			typedef ft::reverse_iterator<iterator>			reverse_iterator;
 			typedef ft::reverse_iterator<const_iterator>	const_reverse_iterator;
 	};
