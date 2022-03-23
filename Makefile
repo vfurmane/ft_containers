ee5e4c98dd73ccec656f02932c7d9bd719f7b8a5
vfurmane <vfurmane@student.42.fr>
2022-03-23 22:39:26 +0100

    feat: add integral_constant and is_integral classes

--- Makefile
+++ Makefile
@@ -6,17 +6,18 @@
 #    By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/03/11 09:51:12 by vfurmane          #+#    #+#              #
-#    Updated: 2022/03/11 12:39:50 by vfurmane         ###   ########.fr        #
+#    Updated: 2022/03/23 14:17:58 by vfurmane         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME			= containers
 SRCS			= test.cpp
 CC				= c++
-CFLAGS			= -Wall -Wextra -Werror -std=c++98
+CFLAGS			= -Wall -Wextra -Werror
 
 all:			std_$(NAME) ft_$(NAME)
 
+ft_$(NAME):		CFLAGS+= -std=c++98
 ft_$(NAME):		$(SRCS)
 				$(CC) $(CFLAGS) -D USE_STD=0 $^ -o ft_$(NAME)
 
---
+++ type_traits.hpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   type_traits.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/23 11:29:47 by vfurmane          #+#    #+#             */
+/*   Updated: 2022/03/23 20:27:53 by vfurmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+namespace ft
+{
+	template <class T, T v>
+	struct integral_constant {
+		const static T value = v;
+		typedef T value_type;
+		typedef integral_constant<T,v> type;
+		operator T()
+		{
+			return v;
+		}
+	};
+
+	typedef integral_constant<bool,true> true_type;
+	typedef integral_constant<bool,false> false_type;
+
+	template <typename T>
+	struct is_integral : public false_type {};
+	template <>
+	struct is_integral<bool> : public true_type {};
+	template<>
+	struct is_integral<char> : public true_type {};
+	template<>
+	struct is_integral<wchar_t> : public true_type {};
+	template<>
+	struct is_integral<signed char> : public true_type {};
+	template<>
+	struct is_integral<short int> : public true_type {};
+	template<>
+	struct is_integral<int> : public true_type {};
+	template<>
+	struct is_integral<long int> : public true_type {};
+	template<>
+	struct is_integral<long long int> : public true_type {};
+	template<>
+	struct is_integral<unsigned char> : public true_type {};
+	template<>
+	struct is_integral<unsigned short int> : public true_type {};
+	template<>
+	struct is_integral<unsigned int> : public true_type {};
+	template<>
+	struct is_integral<unsigned long int> : public true_type {};
+	template<>
+	struct is_integral<unsigned long long int> : public true_type {};
+};
