894bde6978a0304e867309e0b251abc6474c056a
vfurmane <vfurmane@student.42.fr>
2022-04-02 12:00:56 +0200

    feat: add size method

--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 12:05:21 by vfurmane          #+#    #+#             */
-/*   Updated: 2022/04/01 17:20:44 by vfurmane         ###   ########.fr       */
+/*   Updated: 2022/04/02 11:59:48 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,6 +68,11 @@ namespace ft
 				_alloc = x._alloc;
 				_dispatch_ctr(x._arr, x._arr + x._n, ft::false_type());
 				return *this;
+			}
+
+			size_type size() const
+			{
+				return _n;
 			}
 
 			reference operator[] (size_type n)
