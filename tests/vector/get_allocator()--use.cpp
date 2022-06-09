4e727003ca8197c8a4af3c5f3586e96de674ec10
vfurmane <vfurmane@student.42.fr>
2022-06-09 13:50:34 +0200

    test: fix allocator::allocate() call

--- tests/vector/get_allocator()--use.cpp
+++ tests/vector/get_allocator()--use.cpp
@@ -6,7 +6,7 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 12:15:46 by vfurmane          #+#    #+#             */
-/*   Updated: 2022/06/09 12:17:54 by vfurmane         ###   ########.fr       */
+/*   Updated: 2022/06/09 13:49:44 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	main(void)
 	NAMESPACE::vector<int>	obj(5);
 	NAMESPACE::vector<int>::allocator_type	alloc = obj.get_allocator();
 	int	*ptr;
-	alloc.allocate(ptr, 1);
+	ptr = alloc.allocate(1);
 	*ptr = 42;
 	alloc.deallocate(ptr);
 }
