5f106d036bbbc94f366f7438dbf7c6deda4bf3c8
vfurmane <vfurmane@student.42.fr>
2022-06-23 11:12:16 +0200

    feat: add container_type member type

--- tests/stack/container_type--TestHelper.cpp
+++ tests/stack/container_type--TestHelper.cpp
@@ -1,37 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   stack.hpp                                          :+:      :+:    :+:   */
+/*   container_type--TestHelper.cpp                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/22 15:26:49 by vfurmane          #+#    #+#             */
-/*   Updated: 2022/06/23 11:06:38 by vfurmane         ###   ########.fr       */
+/*   Created: 2022/06/22 15:46:04 by vfurmane          #+#    #+#             */
+/*   Updated: 2022/06/23 11:08:50 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef STACK_HPP
-# define STACK_HPP
-
-# include "vector.hpp"
-
-namespace ft {
-	template < class T, class Container = ft::vector<T> >
-	class stack
-	{
-		public:
-			typedef Container container_type;
-
-			stack(const Container& cont = Container()) : c(cont)
-			{
-			}
-			~stack(void)
-			{
-			}
-	
-		protected:
-			Container	c;
-	};
-}
-
+#include <typeinfo>
+#include "tests/TestHelper.hpp"
+#ifdef USE_STD
+# include <stack>
+#else
+# include "stack.hpp"
 #endif
+
+int	main(void)
+{
+	if (typeid(NAMESPACE::stack< int, TestHelper<STACK> >::container_type) == typeid(TestHelper<STACK>))
+		return 0;
+	else
+		return 1;
+}
