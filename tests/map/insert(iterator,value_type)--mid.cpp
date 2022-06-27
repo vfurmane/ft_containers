1eb0944ac29e7dfe733cc4f112dbc20f10e7ea52
vfurmane <vfurmane@student.42.fr>
2022-06-27 22:10:44 +0200

    test: add tests for insert()

---
+++ tests/map/insert(iterator,value_type)--mid.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   insert(iterator,value_type)--mid.cpp               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/27 20:01:57 by vfurmane          #+#    #+#             */
+/*   Updated: 2022/06/27 21:52:28 by vfurmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "tests/print.hpp"
+#ifdef USE_STD
+# include <map>
+#else
+# include "map.hpp"
+#endif
+
+typedef NAMESPACE::pair<const int, float> pair;
+typedef NAMESPACE::map<int, float>::iterator iterator;
+
+int	main(void)
+{
+	pair	arr[] = 
+	{
+		pair(0, 3.1415f),
+		pair(2, 3.1415f),
+		pair(3, 3.1415f),
+		pair(4, 3.1415f),
+		pair(5, 3.1415f)
+	};
+	NAMESPACE::map<int, float>	obj(arr, arr + (sizeof arr / sizeof *arr));
+	iterator ret = obj.insert(++obj.begin(), NAMESPACE::make_pair(1, 3.1415f));
+	std::cout << "size()    : " << obj.size() << std::endl;
+	std::cout << "[1]       : " << obj[1] << std::endl;
+	std::cout << "ret       : " << *ret << std::endl;
+	std::cout << "++begin() : " << *++obj.begin() << std::endl;
+}
